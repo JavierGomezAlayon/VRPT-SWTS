7b6266978ac63cb7570d146df2fe1044f85edc69
Javi <alu0101562445@ull.edu.es>
2025-04-05 17:23:55 +0100

    feat: añadiendo una zona que me faltó por añadir

---
+++ src/Zona/ZonaVertedero/ZonaVertedero.cc
@@ -0,0 +1,25 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: DAA
+// Curso: 3º
+// Práctica 05 : VRPT-SWTS
+// Autor: javier Gómez Alayón
+// Correo: alu0101562445@ull.edu.es
+// Fecha: 04/05/25
+// Archivo ZonaVertedero.cc: Implementación de ZonaVertedero
+//        En este fichero se implementa las funciones de la clase ZonaVertedero
+//
+// Historial de revisiones
+//        04/05/25 - Creación (primera versión) del código
+
+#include"ZonaVertedero.h"
+
+/** ZonaVertedero::ZonaVertedero()
+  * @brief Crea el objeto de la clase ZonaVertedero.
+  * @param 
+  * @return objeto de la clase ZonaVertedero
+  */
+ZonaVertedero::ZonaVertedero() {
+
+}
---
+++ src/Zona/ZonaVertedero/ZonaVertedero.h
@@ -0,0 +1,32 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: DAA
+// Curso: 3º
+// Práctica 05 : VRPT-SWTS
+// Autor: javier Gómez Alayón
+// Correo: alu0101562445@ull.edu.es
+// Fecha: 04/05/25
+// Archivo ZonaVertedero.h : Declaración de la clase ZonaVertedero
+//        En este fichero se declara la clase ZonaVertedero
+//
+// Historial de revisiones
+//        04/05/25 - Creación (primera versión) del código
+#ifndef C_ZonaVertedero_H
+#define C_ZonaVertedero_H
+#include"../../Cordenadas/Cordenadas.h"
+#include"../Zona.h"
+
+/**
+ * @brief Clase que representa una zona de vertedero.
+ * @details Directamente no tiene id, ya que se supone que solo hay un vertedero. 
+ */
+class ZonaVertedero : public Zona {
+ public:
+  ZonaVertedero();
+  int get_id_zona() const;
+ private:
+ 
+};
+
+#endif
